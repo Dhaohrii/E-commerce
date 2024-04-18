72e92049d56be5de652862ee450444f7dacac449
Dhaohrii <dhaohrii@yahoo.com>
2024-04-18 14:38:39 -0300

    Fixing errors in auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -1,34 +1,38 @@
 
-document.getElementById("registration").addEventListener("submit",function(event){
-    event.preventDefault();
-    if(checkExist(document.getElementById("email").value)){
-        alert("Email Registred");
-    }
-    else if(document.getElementById("password").value!==document.getElementById("confirmed").value){
-        alert("Password should be Confirmed Correctly")
-    }
-    else{
-    var info={
-        name:document.getElementById("name").value,
-        email:document.getElementById("email").value,
-        password:document.getElementById("password").value
-    }
-    var data=JSON.stringify(info);
-    localStorage.setItem("UID"+Math.random(),data);
-    alert("Registration Succesful")
-    window.location.replace("login.html")
-    }
-})
+if(document.title.trim() === "Registration"){
+    document.getElementById("registration").addEventListener("submit",function(event){
+        event.preventDefault();
+        if(checkExist(document.getElementById("email").value)){
+            alert("Email Registred");
+        }
+        else if(document.getElementById("password").value!==document.getElementById("confirmed").value){
+            alert("Password should be Confirmed Correctly")
+        }
+        else{
+        var info={
+            name:document.getElementById("name").value,
+            email:document.getElementById("email").value,
+            password:document.getElementById("password").value
+        }
+        var data=JSON.stringify(info);
+        localStorage.setItem("UID"+Math.random(),data);
+        alert("Registration Succesful")
+        window.location.replace("login.html")
+        }
+    });
+}
 
-document.getElementById("login").addEventListener("submit",function(event){
-    event.preventDefault();
-    if(auth(document.getElementById("email").value,document.getElementById("password").value)){
-    window.location.replace("../main/index.html")
-    }
-    else{
-        alert("Email or password false")
-    }
-});
+else if(document.title.trim()=== "Sign in"){
+    document.getElementById("login").addEventListener("submit",function(event){
+        event.preventDefault();
+        if(auth(document.getElementById("email").value,document.getElementById("password").value)){
+        window.location.replace("../main/index.html")
+        }
+        else{
+            alert("Email or password false")
+        }
+    });
+}
 
 function checkExist(searched){
     for(var i=0;i<localStorage.length;i++){
